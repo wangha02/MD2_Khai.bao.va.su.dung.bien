d34af509b58b6d1900ff88d8f1ba87d623aa5731
unknown <btkkentxx2002@gmail.com>
2022-07-24 22:41:29 +0700

    jv

---
+++ src/MD2_B2.java
@@ -0,0 +1,14 @@
+public class MD2_B2 {
+    public static void main(String[] args) {
+        int i = 10; //Khai báo biến i là kiểu int có giá trị là 10;
+        float f = 20.5f; //Khai báo biến f có kiểu là float có giá trị là 20.5
+        boolean b = true; //Khai báo biến b có kiểu là boolean có giá trị là true
+        char c = 'a'; // Khai báo biến c có kiểu là char có giá trị là kí tự a
+        String s = "Hà Nội"; // Khai báo biến s có kiểu là String có giá trị là chuỗi HN;
+        System.out.println("i =" + i);
+        System.out.println("f =" + f);
+        System.out.println("b =" + b);
+        System.out.println("c =" + c);
+        System.out.println("s =" + s);
+    }
+}
